947960ee4a2958da1aa05dcdb983d64879eeca74
Adam Krone <krone.adam@gmail.com>
2013-04-30 10:59:48 -0500

    Adds metadata

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name		  	  "mysql-connector"
+maintainer        "Thirdwave, LLC"
+maintainer_email  "support@thirdwavellc.com"
+license           "Apache 2.0"
+description       "Installs and configures MySQL Connector"
+version           "0.1.0"
