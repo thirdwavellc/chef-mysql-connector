faeb7a60247440190b10761da2b626755d64579b
Orin <orin.fink@thirdwavellc.com>
2014-09-21 12:16:00 -0500

    updates version of connector

--- attributes/liferay.rb
+++ attributes/liferay.rb
@@ -1,3 +1,3 @@
 default['connector']['install_directory'] = "/opt"
-default['connector']['download_version'] = "mysql-connector-java-5.1.29"
-default['connector']['download_url'] = "http://cdn.mysql.com/Downloads/Connector-J/mysql-connector-java-5.1.29.zip"
+default['connector']['download_version'] = "mysql-connector-java-5.1.32"
+default['connector']['download_url'] = "http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.32.zip"
